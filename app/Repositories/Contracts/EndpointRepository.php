ff013d8f6f2f8b14135a5201b72cfe24aac00268
Sykov7 <bryan@taxo.co>
2023-01-16 15:57:21 -0500

    add repositories

---
+++ app/Repositories/Contracts/EndpointRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface EndpointRepository
+{
+    public function all();
+}
---
+++ app/Repositories/Eloquent/EloquentEndpointRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Repositories\Contracts\EndpointRepository;
+
+class EloquenEndpointRepository implements EndpointRepository
+{
+    public function all()
+    {
+        //
+    }
+}
